7c0f21d8b38a09a93a7847361ef5c8b065d47fb7
Yury Belousov <yury-belousov@yandex.ru>
2018-07-22 20:03:28 +0300

    Initial commit

---
+++ app/src/main/java/com/yury/lebowski/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.yury.lebowski
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yury.lebowski.ui.home.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, HomeFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
