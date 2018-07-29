fd053ea0eec9a4d1336eef1288396fba852269b7
Yury Belousov <yury-belousov@yandex.ru>
2018-07-30 01:58:31 +0300

    Add fab with speed dial (#16)
    * add library
    
    * add menu
    
    * add icons

--- app/src/main/java/com/yury/lebowski/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yury/lebowski/ui/home/HomeFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.yury.lebowski.R
 import com.yury.lebowski.databinding.HomeFragmentBinding
 import com.yury.lebowski.util.autoCleared
+import kotlinx.android.synthetic.main.home_fragment.*
 
 
 class HomeFragment : Fragment() {
@@ -29,6 +31,22 @@ class HomeFragment : Fragment() {
                 container,
                 false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        speedDial.inflate(R.menu.menu_speed_dial)
+        speedDial.setOnActionSelectedListener { actionItem ->
+            when (actionItem.id) {
+            R.id.manage_categories -> {
+                Toast.makeText(context, getString(R.string.tbd), Toast.LENGTH_SHORT).show()
+                true
+            }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 
 
